f47ef9979c833ecd5c8d4f9ac910ac4ab5ba2e81
Willy Alers <willy.alers@allheart.net>
2022-02-14 11:55:16 -0800

    24 Using the Native Event Object.

--- Section 2b Understanding Event Binding/app.js
+++ Section 2b Understanding Event Binding/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:''
     };
   },
   methods:{
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName;
+    },
     increment(num){
       this.counter += num;
     },
