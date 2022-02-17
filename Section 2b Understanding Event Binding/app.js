6fa5d789e11a5ddd71fa9bda3b0b77ae99ae4d38
Willy Alers <willy.alers@allheart.net>
2022-02-17 09:17:04 -0800

    27 Data Binding + Event Binding = Two-Way Binding.

--- Section 2b Understanding Event Binding/app.js
+++ Section 2b Understanding Event Binding/app.js
@@ -14,13 +14,16 @@ const app = Vue.createApp({
       this.confirmedName = this.name;
     },
     setName(event, lastName){
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     increment(num){
       this.counter += num;
     },
     decrement(num){
       this.counter -= num;
+    },
+    resetInput(){
+      this.name = '';
     }
   }
 });
