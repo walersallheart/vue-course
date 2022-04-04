e4727f3b24338014217ca85cd1c5becc46a28df6
Willy Alers <willy.alers@allheart.net>
2022-04-04 12:56:06 -0700

    128 First Components and Props.

--- Section 10 The Learning Resources App/src/App.vue
+++ Section 10 The Learning Resources App/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-    <ul></ul>
+    <ul>
+        <learning-resource
+            v-for="res in storedResources"
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        ></learning-resource>
+    </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource';
+
 export default {
+    components:{
+        LearningResource
+    },
     data() {
         return {
             storedResources: [
