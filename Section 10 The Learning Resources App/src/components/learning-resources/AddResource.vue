d679ad39105335fa2115cfaefbc2c15453dfcce6
Willy Alers <willy.alers@allheart.net>
2022-04-04 13:47:57 -0700

    134 Adding a Form.

--- Section 10 The Learning Resources App/src/components/learning-resources/AddResource.vue
+++ Section 10 The Learning Resources App/src/components/learning-resources/AddResource.vue
@@ -1,5 +1,26 @@
 <template>
-    <h2>Add Resource</h2>
+    <base-card>
+        <form>
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input id="title" name="title" type="text" />
+            </div>
+
+            <div class="form-control">
+                <label for="description">Description</label>
+                <textarea id="description" name="description"></textarea>
+            </div>
+
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input id="link" name="link" type="url" />
+            </div>
+
+            <div class="form-control">
+                <base-button type="submit">Add Resource</base-button>
+            </div>
+        </form>
+    </base-card>
 </template>
 
 <script>
@@ -9,5 +30,29 @@ export default {
 </script>
 
 <style scoped>
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
 
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+    outline: none;
+    border-color: #3a0061;
+    background-color: #f7ebff;
+}
+
+.form-control {
+    margin: 1rem 0;
+}
 </style>
