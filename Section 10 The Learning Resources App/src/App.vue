b0b7ee9780e6595d9cc6ba2c406a215f50ef178d
Willy Alers <willy.alers@allheart.net>
2022-04-04 12:35:40 -0700

    127 Setup and First Steps.

---
+++ Section 10 The Learning Resources App/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title:  'Official Guide',
+                    description:'The Official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description:'Learn to Google...',
+                    link: 'https://www.google.com'
+                },
+            ]
+        };
+    }
+}
+</script>
