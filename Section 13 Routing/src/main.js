3f8ea484ad014013c62536eafb8b9f00d2c36161
Willy Alers <willy.alers@allheart.net>
2022-04-05 12:48:01 -0700

    168 Registering & Rendering Routes.

--- Section 13 Routing/src/main.js
+++ Section 13 Routing/src/main.js
@@ -1,7 +1,21 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
-const app = createApp(App)
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/teams/TeamsList.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        { path:'/teams', component: TeamsList },
+        { path:'/users', component: UsersList },
+    ]
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
