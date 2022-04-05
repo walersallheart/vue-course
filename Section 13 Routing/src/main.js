dc3a80abbaf7201d47003925dc295aa4e34a3c42
Willy Alers <willy.alers@allheart.net>
2022-04-05 16:51:49 -0700

    Set team members to show on teams page.

--- Section 13 Routing/src/main.js
+++ Section 13 Routing/src/main.js
@@ -11,8 +11,13 @@ const router = createRouter({
     history: createWebHistory(),
     routes:[
         { path:'/', redirect:'/teams' },
-        { path:'/teams', component: TeamsList },
-        { path:'/teams/:teamId', component: TeamMembers, props: true },
+        {
+            path:'/teams',
+            component: TeamsList,
+            children: [
+                { path: ':teamId', component: TeamMembers, props: true },
+            ] 
+        },
         { path:'/users', component: UsersList },
         { path:'/:notFound(.*)', redirect:'/teams' },
     ]
