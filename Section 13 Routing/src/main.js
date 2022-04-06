42584fab29fb35e9c73e21d0f31defd603194b50
Willy Alers <willy.alers@allheart.net>
2022-04-05 17:48:31 -0700

    188 Organizing Route FIles.

--- Section 13 Routing/src/main.js
+++ Section 13 Routing/src/main.js
@@ -1,51 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import router from './router';
 
 import App from './App.vue';
-
-import TeamsList from './components/teams/TeamsList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes:[
-        { path:'/', redirect:'/teams' },
-        {
-            path:'/teams',
-            components: {
-                default: TeamsList,
-                footer: TeamsFooter
-            },
-            children: [
-                { path: ':teamId', component: TeamMembers, props: true },
-            ] 
-        },
-        { 
-            path:'/users',
-            components:  {
-                default: UsersList,
-                footer: UsersFooter
-            }, 
-        },
-        { path:'/:notFound(.*)', redirect:'/teams' }
-    ],
-    scrollBehavior(to, from, savedPosition) {
-        if (savedPosition) { //this will scroll the user back where they were if they go back
-            return savedPosition;
-        }
-
-        console.log({to});
-        console.log({from});
-        console.log({savedPosition});
-
-        return {
-            left: 0, top: 0
-        }
-    }
-});
 
 const app = createApp(App);
 
