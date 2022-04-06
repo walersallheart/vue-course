f3e7670b01679ea305457d9aacf1e553f5643602
Willy Alers <willy.alers@allheart.net>
2022-04-06 14:22:53 -0700

    215 Passing  Data to Mutations with Payloads.

--- Section 15 Vuex/src/App.vue
+++ Section 15 Vuex/src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <base-container title="Vuex">
-    <the-counter></the-counter>
-    <button @click="addOne">Add 1</button>
+    <the-counter />
+    <button @click="addTen">Add 10</button>
+    <change-counter />
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from  './components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
+    TheCounter,
+    ChangeCounter
   },
   methods: {
-    addOne() {
-      this.$store.state.counter++;
+    addTen() {
+      this.$store.commit({
+        type: 'increase',
+        value: 10
+      });
     }
   }
 };
