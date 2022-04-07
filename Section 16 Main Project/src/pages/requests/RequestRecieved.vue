ce11247e0796346b5ef0a951d61ca16feb09048c
Willy Alers <willy.alers@allheart.net>
2022-04-07 16:46:50 -0700

    249 Outputting Incoming Requests (Messages)

--- Section 16 Main Project/src/pages/requests/RequestRecieved.vue
+++ Section 16 Main Project/src/pages/requests/RequestRecieved.vue
@@ -1,15 +1,53 @@
 <template>
-    <div>
-        REQUESTS
-    </div>
+    <section>
+        <base-card>
+            <header>
+                <h2>Requests Recieved</h2>
+            </header>
+            <ul v-if="hasRequests">
+                <request-item
+                    v-for="request in receivedRequests"
+                    :key="request"
+                    :message="request.message"
+                    :email="request.userEmail"
+                ></request-item>
+            </ul>
+            <h3 v-else>You haven't recieved any requests yet.</h3>
+        </base-card>
+    </section>
 </template>
 
 <script>
+    import RequestItem from '../../components/requests/RequestItem.vue';
+
     export default {
-        
+        components: {
+            RequestItem
+        },
+        computed: {
+            receivedRequests() {
+                return this.$store.getters['requests/requests'];
+            },
+            hasRequests(){
+                return this.$store.getters['requests/hasRequests'];
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+header {
+    text-align: center;
+}
 
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+}
+
+h3 {
+    text-align: center;
+}
 </style>
