2847ed9b990e09fe4ffa0c6b6536cb1f115942d0
Willy Alers <willy.alers@allheart.net>
2022-04-07 13:31:15 -0700

    244 Registering as a Coach: The Form

--- Section 16 Main Project/src/pages/coaches/CoachRegistration.vue
+++ Section 16 Main Project/src/pages/coaches/CoachRegistration.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-        REGISTER
-    </div>
+    <section>
+        <base-card>
+            <h2>Register as a coach</h2>
+            <coach-form></coach-form>
+        </base-card>
+    </section>
 </template>
 
 <script>
+    import CoachForm from '../../components/coaches/CoachForm.vue';
+
     export default {
-        
+        components: {
+            CoachForm
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
